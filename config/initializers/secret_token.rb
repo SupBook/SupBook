c78ca063cc50411081dc4202c20849bfb69aaac9
Florian PETIT <florianp37@me.com>
2012-02-01 11:56:12 +0100

    initialized an empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupBook::Application.config.secret_token = '263867fef4aa6982dd3efaafe5df5fa0f1ca95ba92c51d4494cf2988c47d520a9779635193af674d6c8e5245eee1c743389826299e3dfaebc7aca5786b07cce4'
